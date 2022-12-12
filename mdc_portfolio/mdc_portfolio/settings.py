83e3ee97ed7cb2266b5a85fb9e7a32ed44436172
Miguel Díaz Castillo <miguel.acastillodiaz@outlook.es>
2022-12-12 12:35:47 -0500

    Finaly, railway

--- mdc_portfolio/mdc_portfolio/settings.py
+++ mdc_portfolio/mdc_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ccua4rt)^&x3#8y_sg*p57u2@_&74pra+sr+mh7d74^q!c)8f5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mdc_portfolio/static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
